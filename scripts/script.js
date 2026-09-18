82fe06de188d8d0e12c2ad5bd918bb1f263d9411
Arya Tajne <122556087+aryatajne28@users.noreply.github.com>
2026-09-18 17:37:12 +0000

    Added a function to sum up the area's total time when a new activity is logged

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,12 @@
 
-const areas = ['Coding', 'Art', 'Reading'];
+const areasObjectArray = [{ area: 'Coding', total: 0 }, { area: 'Art', total: 0 }, { area: 'Reading', total: 0 }];
 
 const dropdownElement = document.getElementById('area');
 
-areas.forEach((area) => {
-
+areasObjectArray.forEach((area) => {
     const optionElement = document.createElement('option');
-
-    optionElement.value = area.toLowerCase();
-
-    optionElement.textContent = area;
-
+    optionElement.value = area.area;
+    optionElement.textContent = area.area;
     dropdownElement.appendChild(optionElement);
 })
 
@@ -24,5 +20,11 @@ addForm.addEventListener('submit', (event) => {
 
     const activity = Object.fromEntries(formData);
 
-    console.log(activity);
+    const areaFormValue = formData.get('area');
+
+    const areaValue = areasObjectArray.find((area) => area.area == areaFormValue);
+
+    areaValue.total += Number(formData.get('time'));
+
+    console.log(areasObjectArray);
 })
